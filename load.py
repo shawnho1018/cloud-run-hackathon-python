0246518551ddb0c451d5b517ab7abe7319420332
shawnho@google.com <shawnho@google.com>
2022-05-19 03:09:40 +0000

    update strategy

---
+++ load.py
@@ -0,0 +1,70 @@
+import json
+import logging
+
+logger = logging.getLogger(__name__)
+
+with open('test.json', 'r') as f:
+  #data = ast.literal_eval(f)
+  data = json.load(f)
+
+moves = ['F', 'T', 'L', 'R']
+
+
+
+self = 'https://watersplash-ya6rumdz4a-uc.a.run.app'
+dimension = data['arena']['dims']
+mydir = data['arena']['state'][self].direction
+mycor = [data['arena']['state'][self].x, data['arena']['state'][self].y]
+
+
+
+# should fire
+for user in data['arena']['state']:
+    if user != self: 
+        print('user: %s, data: %s' % (user, data['arena']['state'][user]))
+    
+
+def moveTo(dst_x, dst_y, mydir, my_x, my_y):
+    move = 'None'
+    if my_y > dst_y:
+        if mydir == 'N':
+            move = 'T'
+        elif mydir == 'E':
+            move = 'L'
+        elif mydir == 'W':
+            move = 'R'
+        else:
+            move = 'L'
+    elif my_y < dst_y:
+        if mydir == 'N':
+            move = 'L'
+        elif mydir == 'E':
+            move = 'R'
+        elif mydir == 'W':
+            move = 'L'
+        else:
+            move = 'T'        
+    else:
+        if my_x > dst_x:
+            if mydir == 'N':
+                move = 'L'
+            elif mydir == 'E':
+                move = 'L'
+            elif mydir == 'W':
+                move = 'T'
+            elif mydir == 'S':
+                move = 'R'
+        elif my_x < dst_x:            
+            if mydir == 'N':
+                move = 'R'
+            elif mydir == 'E':
+                move = 'T'
+            elif mydir == 'W':
+                move = 'R'
+            elif mydir == 'S':
+                move = 'L'
+            
+    return move
+
+
+        
--- main.py
+++ main.py
@@ -32,10 +32,58 @@ def index():
 @app.route("/", methods=['POST'])
 def move():
     request.get_data()
-    
-    logger.info(request.json)
+    self = 'https://watersplash-ya6rumdz4a-uc.a.run.app'
+    dimension = data['arena']['dims']
+    mydir = data['arena']['state'][self].direction
+    mycor = [data['arena']['state'][self].x, data['arena']['state'][self].y]
+    result = moveTo(dimension[0], dimension[1], mydir, mycor[0], mycor[1])
 
-    return moves[random.randrange(len(moves))]
+    logger.info(request.json)
+    return result
+    #return moves[random.randrange(len(moves))]
+
+def moveTo(dst_x, dst_y, mydir, my_x, my_y):
+    move = 'None'
+    if my_y > dst_y:
+        if mydir == 'N':
+            move = 'T'
+        elif mydir == 'E':
+            move = 'L'
+        elif mydir == 'W':
+            move = 'R'
+        else:
+            move = 'L'
+    elif my_y < dst_y:
+        if mydir == 'N':
+            move = 'L'
+        elif mydir == 'E':
+            move = 'R'
+        elif mydir == 'W':
+            move = 'L'
+        else:
+            move = 'T'        
+    else:
+        if my_x > dst_x:
+            if mydir == 'N':
+                move = 'L'
+            elif mydir == 'E':
+                move = 'L'
+            elif mydir == 'W':
+                move = 'T'
+            elif mydir == 'S':
+                move = 'R'
+        elif my_x < dst_x:            
+            if mydir == 'N':
+                move = 'R'
+            elif mydir == 'E':
+                move = 'T'
+            elif mydir == 'W':
+                move = 'R'
+            elif mydir == 'S':
+                move = 'L'
+            
+    return move
+
 
 if __name__ == "__main__":
   app.run(debug=False,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
